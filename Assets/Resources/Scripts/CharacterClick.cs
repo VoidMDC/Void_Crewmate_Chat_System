fc4953124d97185822c13f0b12180d8498e80b0e
Austin Acosta <aa6614293@gmail.com>
2017-12-04 16:57:01 -0500

    Feature Lock-in

---
+++ Assets/Resources/Scripts/CharacterClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Yarn.Unity {
+	public class CharacterClick : MonoBehaviour {
+		private DialogueRunner dialogue;
+		public string talkToNode;
+		private Collider coll;
+
+		// Use this for initialization
+		void Start () {
+			dialogue = GameObject.Find("Dialogue").GetComponent<DialogueRunner>();
+			coll = GetComponent<BoxCollider>();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if(dialogue.isDialogueRunning) {
+				coll.enabled = false;
+			} else {
+				coll.enabled = true;
+			}
+		}
+
+		void OnMouseDown() {
+			Debug.Log("Click Detected!");
+			dialogue.StartDialogue(talkToNode);
+		}
+	}
+}
